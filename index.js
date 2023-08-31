d6f68d7df73e0a25aec47be8da6b0d26310da2d2
Matias Falcone <matias@greasemonkeygames.com>
2023-08-31 12:00:18 +1000

    prettier

--- index.js
+++ index.js
@@ -1,53 +1,57 @@
-const { getStakeAddress } = require("./lib/getStake")
-const { getAdaHandleAddress } = require("./lib/handle")
+const { getStakeAddress } = require("./lib/getStake");
+const { getAdaHandleAddress } = require("./lib/handle");
 const stakeTable = require("./lib/db");
-const express = require('express')
-const app = express()
+const express = require("express");
+const app = express();
 const port = process.env.PORT || 3001;
 
-app.use(express.json())
+app.use(express.json());
 app.use(function (req, res, next) {
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
-  res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Access-Control-Allow-Headers');
+  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Content-Type, Access-Control-Allow-Headers"
+  );
   next();
 });
 
-app.get('/getStake', (req, res) => {
+app.get("/getStake", (req, res) => {
   const stakeAddress = req.query.stakeAddress;
-  stakeTable.getStakes(stakeAddress)
-  .then(response => {
-    res.status(200).send(response);
-  })
-  .catch(error => {
-    res.status(500).send(error);
-  })
-})
+  stakeTable
+    .getStakes(stakeAddress)
+    .then((response) => {
+      res.status(200).send(response);
+    })
+    .catch((error) => {
+      res.status(500).send(error);
+    });
+});
 
-app.get('/getStakeAddress', (req, res) => {
+app.get("/getStakeAddress", (req, res) => {
   const adaAddress = req.query.adaAddress;
   getStakeAddress(adaAddress)
-  .then(response => {
-    res.status(200).send(response);
-  })
-  .catch(error => {
-    console.log(error);
-    res.status(500).send(error);
-  })
-})
+    .then((response) => {
+      res.status(200).send(response);
+    })
+    .catch((error) => {
+      console.log(error);
+      res.status(500).send(error);
+    });
+});
 
-app.get('/getAdaHandle', (req, res) => {
+app.get("/getAdaHandle", (req, res) => {
   const handleName = req.query.handleName;
   getAdaHandleAddress(handleName)
-  .then(response => {
-    res.status(200).send(response);
-  })
-  .catch(error => {
-    console.log(error);
-    res.status(500).send(error);
-  })
-})
+    .then((response) => {
+      res.status(200).send(response);
+    })
+    .catch((error) => {
+      console.log(error);
+      res.status(500).send(error);
+    });
+});
 
 app.listen(port, () => {
-  console.log(`App running on port ${port}.`)
-})
+  console.log(`App running on port ${port}.`);
+});
