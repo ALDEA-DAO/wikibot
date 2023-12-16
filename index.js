856835083826f3f05f8c07e3b1775a8078e268f7
avilsmeier <alfred_vilsmeier@hotmail.com>
2023-12-16 03:42:12 +0000

    Enabled algolia chat

--- index.js
+++ index.js
@@ -2,11 +2,9 @@ require("dotenv").config();
 const { Saludo } = require("./lib/responses");
 const { searchWiki } = require('./lib/searchWiki');
 const { InlineSearch } = require('./lib/inlineQuery.js');
+const { algoSearch, manualSearch } = require('./lib/algolia.js');
 const { Telegraf } = require('telegraf');
 const { message } = require('telegraf/filters');
-//const readline = require('readline');
-//const { stdin: input, stdout: output } = require('node:process');
-//const algoliasearch = require('algoliasearch')
 
 const bot = new Telegraf(process.env.BOT_TOKEN)
 bot.start((ctx) => ctx.reply(Saludo))
@@ -20,13 +18,17 @@ bot.command('buscar', (ctx) => {
 	let TGreply = searchWiki(ctx.message.text,ctx)
 })
 
-bot.on("inline_query", async (ctx) => {
+bot.on('inline_query', async (ctx) => {
 	console.log(`CTX:
 	` + ctx 
 	+ `
 	inQuery: 
 	` + ctx.inlineQuery.query);
 	let TGreply = await InlineSearch(ctx.inlineQuery.query,ctx)
+});
+
+bot.on('message', (ctx) => {
+	let TGreply = algoSearch(ctx.message.text,ctx)
 });
 
 bot.launch()
@@ -38,39 +40,6 @@ process.on('warning', (warning) => {
   console.warn(warning.name);    // Print the warning name
   console.warn(warning.message); // Print the warning message
   console.warn(warning.stack);   // Print the stack trace
-});
+})
 
-/*
-const client = algoliasearch(process.env.ALGOLIA_ID, process.env.ALGOLIA_SECRET)
-const index = client.initIndex('aldeaorg');
-
-index.search('Que es blockchain?', {
-  attributesToRetrieve: ['url', 'url_without_anchor', 'content', 'lang', 'image', 'thumb_url', 'thumb_cap'],
-  restrictSearchableAttributes: 'content',
-  hitsPerPage: 2,
-}).then(({ hits }) => {
-  console.log(hits);
-});
-
-
-var userInterface = readline.createInterface({ input, output });
-
-userInterface.prompt();
-
-
-userInterface.on("line", async (input) => {
-  await index
-	.search(input, {
-		attributesToRetrieve: ['url', 'url_without_anchor', 'content', 'lang', 'image', 'thumb_url', 'thumb_cap', '_snippetResult.text'],
-		restrictSearchableAttributes: 'content',
-		hitsPerPage: 2,
-	}).then(({ hits }) => {
-		console.log(hits);
-		userInterface.prompt();
-    })
-		.catch((e) => {
-		console.log(e);
-    });
-});
-
-*/
+manualSearch();
