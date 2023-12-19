c36c635378b0f3760d70659a01e10e28d1182758
avilsmeier <alfred_vilsmeier@hotmail.com>
2023-12-19 22:43:07 +0000

    Multiple fixes, better algolia searching

--- index.js
+++ index.js
@@ -6,7 +6,8 @@ const { algoSearch, manualSearch } = require('./lib/algolia.js');
 const { Telegraf } = require('telegraf');
 const { message } = require('telegraf/filters');
 
-const bot = new Telegraf(process.env.BOT_TOKEN)
+const bot = new Telegraf(process.env.BOT_TOKEN);
+
 bot.start((ctx) => ctx.reply(Saludo))
 bot.help((ctx) => ctx.reply(Saludo))
 
@@ -31,10 +32,10 @@ bot.on('message', (ctx) => {
 	let TGreply = algoSearch(ctx.message.text,ctx)
 });
 
-bot.launch()
+bot.launch();
 // Enable graceful stop
-process.once('SIGINT', () => bot.stop('SIGINT'))
-process.once('SIGTERM', () => bot.stop('SIGTERM'))
+process.once('SIGINT', () => bot.stop('SIGINT'));
+process.once('SIGTERM', () => bot.stop('SIGTERM'));
 
 process.on('warning', (warning) => {
   console.warn(warning.name);    // Print the warning name
